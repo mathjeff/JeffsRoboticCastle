b2ee4afb50c654d975dac62dd5d080b0ec27e6a6
Jeff <gastoj3@rpi.edu>
2011-06-05 20:21:49 -0400

    setup to add companions

---
+++ JeffsRoboticCastle/JeffsRoboticCastle/Companion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Companion : Character
+{
+    public void setFriendToFollow(Character friend)
+    {
+        this.friendToFollow = friend;
+    }
+    public override void updateVelocity(double numSeconds)
+    {
+        if (friendToFollow != null)
+        {
+            // Occasionally, this companion teleports closer to the friend that it is following
+
+        }
+
+        base.updateVelocity(numSeconds);
+    }
+    // private
+    Character friendToFollow;
+
+}
